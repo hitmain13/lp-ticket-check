64407858b2e59f2cb37dc11fd19bf22eecfb4385
Fabio Matsumoto <hide.fabio123@gmail.com>
2025-01-16 19:28:36 -0300

    aumenta timeout para 6s e mantém 15 mins

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     baseUrl: 'https://linkinpark.com',
-    defaultCommandTimeout: 4000
+    defaultCommandTimeout: 6000
   },
 });
